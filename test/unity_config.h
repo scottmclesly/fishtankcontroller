79f22cb3fb312c5d32a9d10d6e9e810e90974f27
isensystech <tech@isensys.com>
2026-01-25 20:13:15 -0500

    Build error fixed

---
+++ test/unity_config.h
@@ -0,0 +1,15 @@
+/**
+ * @file unity_config.h
+ * @brief Unity test framework configuration
+ */
+
+#ifndef UNITY_CONFIG_H
+#define UNITY_CONFIG_H
+
+// Use double for floating point comparisons
+#define UNITY_INCLUDE_DOUBLE
+
+// Enable float support
+#define UNITY_INCLUDE_FLOAT
+
+#endif // UNITY_CONFIG_H
